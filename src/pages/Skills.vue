5b1e30a7a235fd9b36e16ca2d6ebcbf75374af91
sgoudy <shelbygoudy@gmail.com>
2021-12-05 09:26:01 -0500

    Lu pic

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -4,7 +4,7 @@
         <Top />
        <section class="right">
             <figure>
-                <img src="/meOnLu.jpg" alt="Shelby on her pony Pelusa with her horse friend Alex in the background">
+                <img src="../assets/meOnLu.jpg" alt="Shelby on her pony Pelusa with her horse friend Alex in the background">
                 <figcaption>Shelby and Pelusa.</figcaption>
             </figure>
               <a class="resume" href="/ShelbyGoudyResume.pdf" id="resume" download>View My Resume</a><br><br>
